f4a219098a1d4744f89baddf2f00305919b9bb37
rus19023 <rus19023@byui.edu>
2022-05-06 11:39:50 -0400

    fixed grid

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -7,6 +7,7 @@ const newsFeed202202spring = [
         "title": "Unpatched DNS Bug Puts Millions of Routers, IoT Devices at Risk",
         "desc": "In a DNS poisoning attack– also known as DNS spoofing and DNS cache poisoning–an attacker deceives a DNS client into accepting a forged response. This forces a program to perform network communications with an arbitrarily defined endpoint instead of the legitimate one.",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "DNS-Attack.jpg"
@@ -16,6 +17,7 @@ const newsFeed202202spring = [
         "title": "Bad Actors Are Maximizing Remote Everything",
         "desc": "As hybrid work and learning become embedded paradigms in our culture, there are fewer layers of protection between malware and would-be victims. And bad actors are gaining access to more tools to help them pull off their nefarious deeds – like exploit kits. At the same time, the attack surface has rapidly expanded and continues to do so.",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "05/02/2022",
         "accessed": "05 May 2022",
         "img": "Work-from-Home-WFH.jpg"
@@ -25,15 +27,27 @@ const newsFeed202202spring = [
         "title": "Best Password Manager to Use for 2022",
         "desc": "A password manager is essentially an encrypted digital vault that stores secure password login information you use to access apps and accounts on your mobile device, websites and other services. Image by <a href='https://unsplash.com/@moneyphotos'>olieman.eth on Unsplash.com</a>",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "05 May 2022",
         "accessed": "05 May 2022",
         "img": "olieman-eth-q7h8LVeUgFU-unsplash.jpg"
     },
     {
-        "url": "xxxxx",
-        "title": "xxxxx",
-        "desc": "xxxxx",
+        "url": "https://threatpost.com/the-future-of-executive-protection-is-digital/179469/",
+        "title": "The Future of Executive Protection is Digital",
+        "desc": "...it’s no longer enough to surround executives with physical guardrails and James Bond-like security detail. Companies spend millions to protect executives’ physical security and their digital lives at work, but they won’t truly be safe unless they are protected in their personal digital lives, as well. Image by <a href='https://unsplash.com/@robsarm'>Rob Sarmiento on Unsplash.com</a>",
         "week": "03",
+        "sort": "2022-05-02",
+        "date": "04 May 2022",
+        "accessed": "05 May 2022",
+        "img": "rob-sarmiento-MZnuRv1ZASk-unsplash.jpg"
+    },
+    {
+        "url": "https://www.cnbc.com/video/2022/05/04/cybersecurity-firm-cybereason-uncovers-chinese-espionage-campaign.html",
+        "title": "Cybersecurity firm Cybereason uncovers Chinese espionage campaign",
+        "desc": "Video: A top cybersecurity company is pulling back the curtain on a hacking campaign aimed at stealing American intellectual property. CNBC’s Eamon Javers joins ‘Squawk Box’ with the details.",
+        "week": "03",
+        "sort": "2022-05-04",
         "date": "04 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -43,15 +57,7 @@ const newsFeed202202spring = [
         "title": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
-        "date": "02 May 2022",
-        "accessed": "05 May 2022",
-        "img": "xxxx"
-    },
-    {
-        "url": "xxxxx",
-        "title": "xxxxx",
-        "desc": "xxxxx",
-        "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -62,6 +68,7 @@ const newsFeed202202spring = [
         "desc": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -71,6 +78,7 @@ const newsFeed202202spring = [
         "title": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -80,6 +88,7 @@ const newsFeed202202spring = [
         "title": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -89,6 +98,7 @@ const newsFeed202202spring = [
         "title": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -98,6 +108,7 @@ const newsFeed202202spring = [
         "title": "xxxxx",
         "desc": "xxxxx",
         "week": "03",
+        "sort": "2022-05-02",
         "date": "02 May 2022",
         "accessed": "05 May 2022",
         "img": "xxxx"
@@ -108,6 +119,7 @@ const newsFeed202202spring = [
         "desc": "Shows cyber attacks around the world in real time",
         "date": "02 May 2022",
         "week": "03",
+        "sort": "2022-05-02",
         "accessed": "05 May 2022",
         "img": "xxxx"
     }
@@ -119,8 +131,8 @@ function createNav(array, id) {
       const container = document.getElementById(id);
       // sort array by name
       array.sort(function(a, b) {
-        var nameA = a.date + a.title; // make non-case-sensitive
-        var nameB = b.date + a.title; // make non-case-sensitive
+        var nameA = a.week + a.sort + a.title; // make non-case-sensitive
+        var nameB = b.week + b.sort + a.title; // make non-case-sensitive
         if (nameA < nameB) {
           return -1;
         }
@@ -131,7 +143,7 @@ function createNav(array, id) {
         return 0;
       });
       // create link list element
-      let menu = '<div>';
+      let menu = '<div id="newsfeed-container">';
       // get list of files to create links for each menu entry
       array.forEach((element) => {
             // TODO: save as variable for adding to index.html page as parameter? (${element.tech})
@@ -144,7 +156,7 @@ function createNav(array, id) {
             menu += `<h6><a href="${element.url}">${element.title}</a></h6>`;
             menu += `<p>Article Date: ${element.date} Accessed: ${element.accessed}</p>`;
             menu += `<p>"${element.desc}"</p>`;
-            menu += `</div> </div> </div> <br>`;
+            menu += `</div> </div> </div> `;
       });
       menu += "</div>";
       container.innerHTML = menu;
